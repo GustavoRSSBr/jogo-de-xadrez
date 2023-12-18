59b5cda9d07884c08414ca2d258135ae8b61cc6d
unknown <gustavo.rodriguesssbr@gmail.com>
2023-12-18 14:00:07 -0300

    criando a classe tabuleiro e peça

--- application/Program.java
+++ application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
 import application.tabuleiro.Posicao;
+import application.tabuleiro.Tabuleiro;
 
 public class Program {
     public static void main(String[] args) {
-        Posicao posicao = new Posicao(10, 5);
-        System.out.println(posicao);
+        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
     }
 }
